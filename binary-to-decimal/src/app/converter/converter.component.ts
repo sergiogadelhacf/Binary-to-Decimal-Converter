7e34c5b1e68529c1ff641f99b572d3358750dba6
sergiogadelhacf <11345330+sergiogadelhacf@users.noreply.github.com>
2020-05-03 18:21:25 -0300

    Adjusted Bootstrap Layout
    Added new bootstrap classes
    Replaced p tags to placeholders
    Added style to button

--- binary-to-decimal/src/app/converter/converter.component.ts
+++ binary-to-decimal/src/app/converter/converter.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ConverterComponent implements OnInit {
 
-  binaryValue: number = 0;
-  decimalValue: number = 0;
+  binaryValue: number;
+  decimalValue: number;
 
   constructor() { }
 
